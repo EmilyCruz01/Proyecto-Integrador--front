3afabdef57f2e504efae1d8105285aa33dc94087
EmilyCruz01 <96510519+EmilyCruz01@users.noreply.github.com>
2024-07-22 01:14:56 -0600

    crearCuenta y cambios en tabla

--- integrador/src/pages/CrearCuenta.jsx
+++ integrador/src/pages/CrearCuenta.jsx
@@ -7,14 +7,19 @@ const CrearCuenta = () => {
       <div className="circulo-inferiorCrear"></div>
       <h2>Crear cuenta:</h2>
       <form>
+      <div className="input-groupCrear">
+          <input type="text" placeholder="Nombre" className="inputCrear" />
+          <input type="text" placeholder="Apellido paterno" className="inputCrear" />
+        </div>
         <div className="input-groupCrear">
-          <input type="email" placeholder="Correo" className="inputCrear" />
+          <input type="text" placeholder="Apellido materno" className="inputCrear" />
+          <input type="tel" placeholder="Telefóno" className="inputCrear" />
+        </div>
+        <div className="input-groupCrear">
+        <input type="email" placeholder="Correo" className="inputCrear" />
           <input type="password" placeholder="Contraseña" className="inputCrear" />
         </div>
-        <div className="input-groupCrear">
-          <input type="text" placeholder="Usuario" className="inputCrear" />
-          <input type="password" placeholder="Confirmar contraseña" className="inputCrear" />
-        </div>
+
         <button type="submit" className="botonCrear">Crear</button>
       </form>
     </div>
