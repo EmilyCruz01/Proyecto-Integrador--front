30d21d2d0c4f46ffc9f8a960e46c4b6493a36922
kato223272 <223272@ids.upchiapas.edu.mx>
2024-07-22 01:08:14 -0600

    cambios de probabilidades

--- integrador/src/pages/GraficaCirculo.jsx
+++ integrador/src/pages/GraficaCirculo.jsx
@@ -41,8 +41,51 @@ const GraficaCirculo = () => {
       return (
         <>
         <Navbar></Navbar>
-        <div className="pie-chart-container">
+        <div style={{display:'flex', width:'100%'}}>
+        
+        <div style={{width:'50%',margin:'2%'}}>
+        <div className='tituloCirculo'><h2>Función multinomial:</h2></div>
+        <div className="datosProbCirculo" style={{display:'flex'}}>
+          <div className='labelProb'>
+          <h3>Verdes:</h3>
+          <input type="text" />
+         
+          </div>
+          <div className='labelProb'>
+          <h3>Maduros:</h3>
+          <input type="text" />
+         
+          </div>
+          <div className='labelProb'>
+          <h3>Pasados:</h3>
+          <input type="text" />
+         
+          </div>
+        <button className='botonGrafica'> Calcular</button>
+        </div>
+
+        <div>
+        <div className='titulo2Circulo'><h2>Probabilidad:</h2></div>
+        <div className="datosProbCirculo" style={{display:'flex'}}>
+          <div className='labelProb'>
+          <h3>Verdes:</h3>
+          <label htmlFor="" className='probVerdes'>datos verdes</label>
+          </div>
+          <div className='labelProb'>
+          <h3>Maduros:</h3>
+          <label htmlFor="" className='probMaduros'>datos maduros</label>
+          </div>
+          <div className='labelProb'>
+          <h3>Pasados:</h3>
+          <label htmlFor="" className='probPasados'>datos pasados</label>
+          </div>
+        <button className='botonGrafica'> Calcular</button>
+        </div>
+        </div>
+        </div>
+        <div className="pie-chart-container" style={{width:'100%', margin:'2%'}}>
           <Pie data={data} options={options} plugins={[ChartDataLabels]} />
+        </div>
         </div>
         </>
       );
