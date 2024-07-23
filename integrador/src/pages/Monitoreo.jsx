3ef7291c3e5489e33aa005adfe9f9a98a4b2d757
kato223272 <223272@ids.upchiapas.edu.mx>
2024-07-23 03:18:37 -0600

    cambios de monitoreo

--- integrador/src/pages/Monitoreo.jsx
+++ integrador/src/pages/Monitoreo.jsx
@@ -1,13 +1,15 @@
 import Navbar from '../components/Menu/Navbar';
-import Camara from '../components/Monitoreo/Camara';
 import Tabla from '../components/Monitoreo/Tabla';
 import '../styles/pages.style/monitoreo.css';
-import { useState, useEffect } from 'react';
+import { useRef, useState,useEffect } from 'react';
 import io from "socket.io-client";
 
 const Monitoreo = () => {
   const [verdesData, setVerdesData] = useState([]);
   const [madurosData, setMadurosData] = useState([]);
+
+  const videoRef = useRef(null);
+  const [stream, setStream] = useState(null);
 
   useEffect(() => {
     const socket = io("https://socket-server.dreamapp.com.mx");
@@ -26,17 +28,37 @@ const Monitoreo = () => {
     };
   }, []);
 
-  const videoSrc = 'http://ruta-video'; 
+  const handleStartCamera = () => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+        setStream(stream);
+      })
+      .catch((err) => {
+        console.error("Error accessing camera: ", err);
+      });
+  };
+
+  const handleStopCamera = () => {
+    if (stream) {
+      stream.getTracks().forEach(track => track.stop());
+      videoRef.current.srcObject = null;
+      setStream(null);
+    }
+  };
 
   return (
     <div>
       <Navbar />
       <div className="monitoreo">
-        <Camara src={videoSrc} />
+        <button onClick={handleStartCamera}>Ver</button>
+        <button onClick={handleStopCamera}>Detener</button>
+        <video ref={videoRef} style={{ width: '100%', height: 'auto' }} autoPlay></video>
       </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <div style={{margin: '40%'}}><Tabla titulo="Verdes" data={verdesData} /></div>
-        <div style={{margin: '40%'}}><Tabla titulo="Maduros" data={madurosData} /></div>
+      <div className='monitoreoTablas' style={{display: 'flex'}}>
+        <div className='tablaVerde'  ><Tabla titulo="Verdes" data={verdesData} /></div>
+        <div className='tablaMaduro' ><Tabla titulo="Maduros" data={madurosData} /></div>
       </div>
     </div>
   );
